6fd83a01c78c5742b93d05a656990cedee63fd61
YANGMINSUK <121544294+minseok06@users.noreply.github.com>
2023-09-18 16:13:19 +0900

    웹 사이트 만들기

--- 2023-09-18/modify_form.php
+++ 2023-09-18/modify_form.php
@@ -49,17 +49,51 @@
 </head>
 <body>
 <?php
-    section_start();
+    session_start();
     if(isset($_SESSION['userid']))
         $userid = $_SESSION['userid'];
     else
         $userid = '';
 
     $con = mysqli_connect("localhost","root","","sample");
-    $sql = "select * form members where id='$userid'";
+    $sql = "select * from members where id='$userid'";
     $result = mysqli_query($con, $sql);
     $row = mysqli_fetch_assoc($result);
+
+    $pass = $row['pass'];
+    $name = $row['name'];
+    $email = $row['email'];
+
+    mysqli_close($con);
 ?>
-    
+    <form name='member' action="modify.php?id=<?=$userid?>" method='post'>
+        <h2>회원 정보 수정</h2>
+    <ul class="join_form">
+        <li>
+            <span class="col1">아이디</span>
+            <span class="col2"><?=$userid?></span>
+        </li>
+        <li>
+            <span class="col1">비밀번호</span>
+            <span class="col2"><input type="password" name="pass" value="<?=$pass?>"></span>
+        </li>
+        <li>
+            <span class="col1">비밀번호 확인</span>
+            <span class="col2"><input type="password" name="pass_confirm"></span>
+        </li>
+        <li>
+            <span class="col1">이름</span>
+            <span class="col2"><input type="text" name="name" value="<?=$name?>"></span>
+        </li>
+        <li>
+            <span class="col1">이메일</span>
+            <span class="col2"><input type="text" name="email" value="<?=$email?>"></span>
+        </li>
+    </ul>
+        <ul class="buttons">
+            <li><button type="button" onclick="check_input()">저장하기</button></li>
+            <li><button type="button" onclick="reset_form()">취소하기</button></li>
+        </ul>
+    </form>
 </body>
 </html>
