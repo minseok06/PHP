cfb5a06768e144eb2b0f331fde31b671253b337c
양민석 | Yang Minseok <121544294+minseok06@users.noreply.github.com>
2024-08-28 12:30:17 +0900

    0828

---
+++ 응프개 2차 수행평가/action_page.php
@@ -0,0 +1,10 @@
+<h1>환영합니다!!</h1>
+
+아이디 : <?php echo $_POST['uid']; ?> <br>
+비밀번호 : <?php echo $_POST['upwd']; ?> <br>
+졸업년도 : <?php echo $_POST['uyear']; ?> <br>
+학번 : <?php echo $_POST['uhakbun']; ?> <br>
+폰번호 : <?php echo $_POST['utel']; ?> <br>
+이메일 : <?php echo $_POST['uemail']; ?> <br>
+집주소 : <?php echo $_POST['uaddr']; ?> <br>
+기타: <?php echo $_POST['uetc']; ?> <br>
